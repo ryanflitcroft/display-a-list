1290bb159af6079304e4abf71240b22107487aad
Ryan Flitcroft <ryanflitcroft@ymail.com>
2021-12-07 14:07:14 -0800

    renderHello function written for helloWorld string array

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
+import { helloWorld } from './hello.js';
+import { renderHello } from './render-hello.js';
 import { renderPets } from './render-pets.js';
 import { pets } from './pets.js';
-import { helloWorld } from './hello.js';
 
 const stringElement = document.getElementById('string-element');
 const objectElement = document.getElementById('object-element');
 
+for (let hello of helloWorld) {
+    const myHello = renderHello(hello);
+    stringElement.append(myHello);
+}
+
 for (let pet of pets) {
     const petContent = renderPets(pet);
     objectElement.append(petContent);
-}
-
-for (let hello of helloWorld) {
-    const helloContainer = document.createElement('div');
-    const helloItem = document.createElement('p');
-    
-    helloItem.textContent = hello;
-    helloContainer.classList.add('list');
-    helloContainer.append(helloItem);
-
-    stringElement.append(helloContainer);
 }
