1d71cd21f4637a400c8190bb6f9b378986f272e7
Ryan Flitcroft <ryanflitcroft@ymail.com>
2021-12-07 12:49:09 -0800

    renderpets function written for pets.js

--- app.js
+++ app.js
@@ -7,23 +7,8 @@ const objectElement = document.getElementById('object-element');
 // console.log(pets);
 
 for (let pet of pets) {
-// for each object in pets array:
-    // create petContainerEl div
-    // create petPropertyEl p 
-    const petContainerEl = document.createElement('div');
-    const petPropertyEl = document.createElement('p');
 
-    // text content of petPropertyEl p is a string pet.type pet.name pet.age
-    petPropertyEl.textContent = `I have a ${pet.type} named ${pet.name}, and their age is ${pet.age}`;
-    // console.log(petPropertyEl);
+    const petContent = renderPets(pet);
 
-    // add class of list to petContainerEl div
-    petContainerEl.classList.add('list');
-    
-    // append petPropertyEl p to petContainerEl div
-    petContainerEl.append(petPropertyEl);
-    // console.log(petContainerEl);
-
-    objectElement.append(petContainerEl);
-
+    objectElement.append(petContent);
 }
